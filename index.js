3622171977652b07ba467fe58d9c91264444428a
Teemu Eskola <teemu.eskola@gmail.com>
2017-08-01 18:59:36 +0300

    static html

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const express = require('express')
 const app = express()
-app.get('/', (req, res) => {
-  res.send('HEYhey!')
-})
+
+app.use(express.static('public'))
 app.listen(3000, () => console.log('Server running on port 3000'))
